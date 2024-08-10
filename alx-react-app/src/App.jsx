2d3e302dcbafe920c20e6552d0c2bcd86749d2a2
unknown <mounir17xm@gmail.com>
2024-08-10 23:59:53 +0100

    add all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,13 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
       <div>
+      <Header />
+        <MainContent />
+            <Footer />
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
