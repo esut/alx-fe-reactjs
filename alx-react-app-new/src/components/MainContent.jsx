5c7fc6d9035894a2e50e6936a0c85da18407cfb9
unknown <mounir17xm@gmail.com>
2024-08-18 21:36:29 +0100

    Add Directory alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+
+    
+    return (
+        <main style={{padding: '20px', fontSize: '18px', lineHeight: '1.6', 
+            color: 'dimgray', backgroundColor: 'whitesmoke', 
+            borderRadius: '8px', margin: '20px auto', 
+            maxWidth: '800px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+
+
+export default MainContent;
