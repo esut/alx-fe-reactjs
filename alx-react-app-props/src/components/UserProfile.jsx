3ab46c214f3f8545edd716b59f5db5ec50e7e21b
esut <mounir17xm@gmail.com>
2024-08-26 01:26:38 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,21 @@
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
-import React from 'react';
-import UserDetails from './UserDetails';
+function UserProfile() {
 
-function UserInfo() {
-  return <UserDetails />;
+
+    
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
 }
 
-export default UserInfo;
+export default UserProfile;
+
