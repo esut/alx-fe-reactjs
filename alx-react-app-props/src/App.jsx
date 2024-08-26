f6799a720416421f30025db171836c9d54ea57a1
esut <mounir17xm@gmail.com>
2024-08-26 01:24:57 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,23 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
-
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import UserContext from './components/UserContext'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
+  const userData = {
+    name: "Alice",
+    age: "25",
+    bio: "Loves hiking and photography"
+  };
 
+  return (
+    <UserContext.Provider value={userData}> 
       <div>
-      <Header />
+        <Header />
         <MainContent />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <Footer />
+        <UserProfile /> 
+        <Footer />
 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -41,7 +46,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </UserContext.Provider>
   )
 }
 
