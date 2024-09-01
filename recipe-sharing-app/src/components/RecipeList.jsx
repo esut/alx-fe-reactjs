84cb7156e33092819f2c1b79d2ccf454fd4608be
esut <mounir17xm@gmail.com>
2024-09-01 22:23:25 +0100

    add filse searchbar

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,28 @@
+import React, { useEffect } from 'react';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore(state => state.filteredRecipes);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  useEffect(() => {
+
+    filterRecipes(); 
+
+  }, [filterRecipes]);
 
   return (
+
     <div>
+
       {recipes.map(recipe => (
+
         <div key={recipe.id}>
+
           <h3>{recipe.title}</h3>
+
           <p>{recipe.description}</p>
+
         </div>
       ))}
     </div>
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,13 +1,18 @@
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (recipeId) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
-  updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe)
-  })),
-  setRecipes: (recipes) => set({ recipes })
-}));
 
-export default useRecipeStore;
+  recipes: [],
+  searchTerm: '',
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+
+  filterRecipes: () => set(state => ({
+
+    filteredRecipes: state.recipes.filter(recipe =>
+      
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
+}));
