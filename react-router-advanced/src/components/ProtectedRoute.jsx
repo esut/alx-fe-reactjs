f1abbab40787e8f80c796605d970b32e6239e571
esut <mounir17xm@gmail.com>
2024-09-02 00:02:32 +0100

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({ element }) {
-  const isAuthenticated = false; 
-  return isAuthenticated ? element : <Navigate to="/" />;
-}
+const isAuthenticated = () => {
+  return true; 
+};
+
+const ProtectedRoute = ({ element }) => {
+  return isAuthenticated() ? element : <Navigate to="/" />;
+};
 
 export default ProtectedRoute;
