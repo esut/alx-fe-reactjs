618e75afb2ec1ff16fab777a7c7cfe71d339f5f1
esut <mounir17xm@gmail.com>
2024-09-02 00:00:15 +0100

    jj

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 function BlogPost() {
   const { id } = useParams(); 
-  return <h2>Blog Post {id}</h2>; 
+  return <h2>Blog Post {id}</h2>;
 }
 
 export default BlogPost;
