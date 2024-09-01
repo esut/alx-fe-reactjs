2e040feabea068743f076b08d1975cb40bf99306
esut <mounir17xm@gmail.com>
2024-09-01 22:15:56 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,15 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
 function App() {
 
 
   return (
-    <div>
-    <h1> Recipe Sharing app </h1>
-    <AddRecipeForm />
-    <RecipeList />
-  </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
