283df5218e818bdd5470069e271b3825ec2deeb7
esut <mounir17xm@gmail.com>
2024-09-01 22:30:05 +0100

    add all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,23 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+
+import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-function App() {
+import FavoritesList from './components/FavoritesList';
 
+import RecommendationsList from './components/RecommendationsList';
 
+const App = () => {
   return (
     <Router>
+
       <Routes>
+
         <Route path="/" element={<RecipeList />} />
         <Route path="/add" element={<AddRecipeForm />} />
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="/recommendations" element={<RecommendationsList />} />
+        
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
