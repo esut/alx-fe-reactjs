155d789512eec2b49487171655bab17fe2a859bf
esut <mounir17xm@gmail.com>
2024-09-01 23:18:44 +0100

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,13 @@
-
 import React from 'react';
 import PostsComponent from './PostsComponent';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <div className="App">
       <h1>Posts</h1>
       <PostsComponent />
+      <ReactQueryDevtools initialIsOpen={false} />
     </div>
   );
 }
