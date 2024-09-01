33f5986e605943fc5341d24f84603d4aeb128949
esut <mounir17xm@gmail.com>
2024-09-01 21:37:36 +0100

    add agin

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+function App() {
+
+
+  return (
+    <div>
+    <h1> Recipe Sharing app </h1>
+    <AddRecipeForm />
+    <RecipeList />
+  </div>
+  )
+}
+
+export default App
