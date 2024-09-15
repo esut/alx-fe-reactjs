5887c2f03dc27bcf32a1f5ad536317f941674ad7
esut <mounir17xm@gmail.com>
2024-09-15 23:55:05 +0100

    3

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
+import './App.css';
 
+import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import FavoritesList from './components/FavoritesList';
-
 import RecommendationsList from './components/RecommendationsList';
+import SearchBar from './components/SearchBar'; 
 
 const App = () => {
   return (
     <Router>
-
-      <Routes>
-
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-        <Route path="/favorites" element={<FavoritesList />} />
-        <Route path="/recommendations" element={<RecommendationsList />} />
-        
-      </Routes>
+      <div className="App">
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,24 +2,45 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
   favorites: [],
   recommendations: [],
 
   addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe]
+    recipes: [...state.recipes, recipe],
+    filteredRecipes: [...state.filteredRecipes, recipe] // Update filtered recipes too
   })),
 
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map((recipe) =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+    filteredRecipes: state.filteredRecipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
 
   deleteRecipe: (recipeId) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    filteredRecipes: state.filteredRecipes.filter((recipe) => recipe.id !== recipeId)
   })),
 
-  setRecipes: (recipes) => set({ recipes }),
+  setRecipes: (recipes) => set(() => ({
+    recipes,
+    filteredRecipes: recipes 
+  })),
+
+  setSearchTerm: (term) => set((state) => {
+    const newTerm = term.toLowerCase();
+    const filteredRecipes = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(newTerm) ||
+      recipe.ingredients.some(ingredient =>
+        ingredient.toLowerCase().includes(newTerm)
+      )
+    );
+    return { searchTerm: newTerm, filteredRecipes };
+  }),
 
   addFavorite: (recipeId) => set((state) => ({
     favorites: [...state.favorites, recipeId]
