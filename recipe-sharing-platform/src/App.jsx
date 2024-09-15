e9ae0219f8da037f87d58a2adbf799b5be92bd6d
esut <mounir17xm@gmail.com>
2024-09-15 22:46:33 +0100

    recipe-sharing-platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-500 text-4xl">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
