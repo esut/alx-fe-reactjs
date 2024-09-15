c1adb69904442fd344f614dc05e67dd49384086c
esut <mounir17xm@gmail.com>
2024-09-16 00:08:39 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,17 @@
-
 import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl">
       <img
         src="profile-image.jpg"
         alt="User"
-        className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
+        className="w-36 h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+      <h1 className="text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
         Mounir
       </h1>
-      <p className="text-sm sm:text-base md:text-lg text-gray-600">
+      <p className="text-base text-gray-600">
         Developer at Alx Africa. Loves to write code and explore new technologies.
       </p>
     </div>
