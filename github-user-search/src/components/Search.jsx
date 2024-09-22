8293086d6fa54c1b13c5788aefe57570f44f91b6
esut <mounir17xm@gmail.com>
2024-09-22 21:57:41 +0100

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import  { fetchUserData }  from '../services/githubService';
+import React , {useState} from 'react';
+import  { fetchUserData } from '../services/githubService'
+import axios from 'axios';
 
-const Search = () => {
+const Search = () => { 
 
-    const [username] = useState('');
+    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
 
+const handleInputChange = (event) => { 
+    setUsername(event.target.value);
+ };
 
-
-
-
+const handleSubmit = async (event) => { 
+    event.preventDefault();
+ };
 };
-
 export default Search;
