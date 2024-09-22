887daa8c6fd9e5e08d26f17096040448135c20db
esut <mounir17xm@gmail.com>
2024-09-22 20:51:25 +0100

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Search from './components/Search';
 function App() {
   
 
   return (
     <div className='App'>
         <h1>GitHub user search</h1>
+        <Search />
     </div>
   );
 }
