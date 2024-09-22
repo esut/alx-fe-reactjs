c432ea2c90b1dc8598685ed017e0db81a9e8c513
esut <mounir17xm@gmail.com>
2024-09-22 21:07:28 +0100

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import  fetchUserData  from '../services/githubService';
+import  { fetchUserData }  from '../services/githubService';
+
+const Search = () => {
+
+    const [username] = useState('');
 
 
 
 
 
-
-
-
-
-
+};
 
 export default Search;
