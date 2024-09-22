e4a7feacb43ae5474ecaaf9c578c1ff0717d66a9
esut <mounir17xm@gmail.com>
2024-09-22 21:02:25 +0100

    fix

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import  fetchUserData  from '../services/githubService';
+
+
+
+
+
+
+
+
+
+
+
+export default Search;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 export const fetchUserData = async (username) => {
 
     const response = await axios.get(`https://api.github.com/users/${username}`);
-    
+
     return response.data;
+
   };
 
 
