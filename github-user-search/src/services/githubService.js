22961a09aeb1dc65cd5a1a0ae78e071b983859e5
esut <mounir17xm@gmail.com>
2024-09-22 20:55:46 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+export const fetchUserData = async (username) => {
 
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    
+    return response.data;
+  };
 
 
 
