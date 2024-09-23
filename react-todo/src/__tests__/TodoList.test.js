b6c775f4662d0cba462b8ddfff3832ddd8629840
esut <mounir17xm@gmail.com>
2024-09-23 15:08:20 +0100

    fix all

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import TodoList from '../TodoList';
+
+test('renders TodoList component', () => {
+  render(<TodoList />);
+});
+
